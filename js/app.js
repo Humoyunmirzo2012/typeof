858d2d76d336f05dd2ef56b797c8aa9b47dfe987
Humoyunmirzo2012 <webdevoper0509@gmail.com>
2024-08-05 18:48:49 +0500

    typeof

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// let value = [1, 2, 3];
+// console.log(value);
+// console.log(typeof value);
+
+// let value = null;
+// console.log(value);
+// console.log(typeof value);
+
+// let func = function() {};
+// console.log(typeof func);
+
+// let num = 10;
+// let str = "10";
+// console.log(typeof (num + str)); //1010
+
+// let array = [1, 2, 3];
+// let object = { a: 1, b: 2 };
+// console.log(typeof (array + object)); //1,2,3, object,object
+
+let undefinedValue;
+let nullValue = null;
+console.log(typeof  undefinedValue, typeof nullValue); //underfined object
