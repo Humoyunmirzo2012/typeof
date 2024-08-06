af64429322da44c4999f77b40dcd65a9a457ebaf
Humoyunmirzo2012 <webdevoper0509@gmail.com>
2024-08-06 14:03:37 +0500

    taypiof

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-// let value = [1, 2, 3];
-// console.log(value);
-// console.log(typeof value);
+// // let value = [1, 2, 3];
+// // console.log(value);
+// // console.log(typeof value); //typeof object
 
-// let value = null;
-// console.log(value);
-// console.log(typeof value);
+// // let value = null;
+// // console.log(value);
+// // console.log(typeof value); //typeof object
 
-// let func = function() {};
-// console.log(typeof func);
+// // let func = function() {};
+// // console.log(typeof func); //typeof function
 
-// let num = 10;
-// let str = "10";
-// console.log(typeof (num + str)); //1010
+// // let num = 10;
+// // let str = "10";
+// // console.log(typeof (num + str)); //1010 typeof string
 
-// let array = [1, 2, 3];
-// let object = { a: 1, b: 2 };
-// console.log(typeof (array + object)); //1,2,3, object,object
+// // let array = [1, 2, 3];
+// // let object = { a: 1, b: 2 };
+// // console.log(typeof (array + object)); //typeof string
 
-let undefinedValue;
-let nullValue = null;
-console.log(typeof  undefinedValue, typeof nullValue); //underfined object
+// let undefinedValue;
+// let nullValue = null;
+// console.log(typeof  undefinedValue, typeof nullValue); //typeof underfined object
